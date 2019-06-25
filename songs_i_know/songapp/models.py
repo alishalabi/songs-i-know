f076418beff35056e70f6ed07bd1771944327af1
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-06-25 14:31:34 -0700

    Added first model, completed first migration, preparing to add superuser

--- songs_i_know/songapp/models.py
+++ songs_i_know/songapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+SKILL_CHOICES = (
+    ('novice', 'Novice'),
+    ('intermediate', 'Intermediate'),
+    ('advanced', 'Advanced'),
+    ('black_belt', 'Black Belt'),
+)
+
 # Create your models here.
+
+
+class Song(models.Model):
+    song_name = models.CharField(max_length=200)
+    youtube_url = models.CharField(max_length=300, blank=True, default='')
+    song_tab = models.CharField(max_length=100, blank=True, default='')
+    lyrics = models.CharField(max_length=2000, blank=True, default='')
+    skill_level = models.CharField(
+        max_length=200, choices=SKILL_CHOICES, default='intermediate')
+
+    def __str__(self):
+        return self.song_name
