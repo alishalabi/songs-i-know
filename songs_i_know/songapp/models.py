c905abab65a1797dcce7d6c7c88ea4a443bdd0b3
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-06-26 13:26:22 -0700

    Added first view + template, have song index route working

--- songs_i_know/songapp/models.py
+++ songs_i_know/songapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-SKILL_CHOICES = (
+EXPERIENCE_CHOICES = (
     ('novice', 'Novice'),
     ('intermediate', 'Intermediate'),
     ('advanced', 'Advanced'),
@@ -17,8 +17,8 @@ class Song(models.Model):
         max_length=300, blank=True, default='')
     song_tab = models.CharField(max_length=100, blank=True, default='')
     lyrics = models.CharField(max_length=2000, blank=True, default='')
-    skill_level = models.CharField(
-        max_length=200, choices=SKILL_CHOICES, default='intermediate')
+    experience_level = models.CharField(
+        max_length=200, choices=EXPERIENCE_CHOICES, default='intermediate')
 
     def __str__(self):
         return self.song_name
--- songs_i_know/songapp/views.py
+++ songs_i_know/songapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Song
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Query all songs
+    all_songs = Song.objects.all()
+    # template = loader.get_template('songapp/index.html')
+    context = {
+        'all_songs': all_songs,
+    }
+    # output = ', '.join([s.song_name for s in all_songs])
+    return render(request, 'songapp/index.html', context)
+
+
+def detail(request, song_id):
+    return HttpResponse("You're looking at song %s." % song_id)
