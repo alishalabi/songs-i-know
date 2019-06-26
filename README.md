76b4ab779b032c28012c4e9e8ae8e852b8b9e40f
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-06-26 14:37:57 -0700

    Refactored views to generic django views

---
+++ README.md
@@ -0,0 +1,14 @@
+<h4>Technologies</h4>
+<ul>
+  <li>Virtualenv - `pip3 install virtualenv`</li>
+  <li>Django - `pip3 install Django`</li>
+  <li></li>
+</ul>
+
+<h4>Django Modules</h4>
+<ul>
+  <li>django.http -> HttpResponse</li>
+  <li>django.shortcuts -> get_object_or_404</li>
+  <li>django.urls -> generic</li>
+  <li></li>
+</ul>
--- songs_i_know/songapp/views.py
+++ songs_i_know/songapp/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Song
 
 
-def index(request):
-    # Query all songs
-    all_songs = Song.objects.all()
-    # template = loader.get_template('songapp/index.html')
-    context = {
-        'all_songs': all_songs,
-    }
-    # output = ', '.join([s.song_name for s in all_songs])
-    return render(request, 'songapp/index.html', context)
+class IndexView(generic.ListView):
+    # model = Song
+    template_name = 'songapp/index.html'
+    context_object_name = 'all_songs'
+    # all_songs = Song.objects.all()
+    # context = {
+    #     'all_songs': all_songs,
+    # }
+
+    def get_queryset(self):
+        return Song.objects.all()
 
 
-def detail(request, song_id):
-    song = get_object_or_404(Song, pk=song_id)
-    return render(request, 'songapp/detail.html', {'song': song})
+class DetailView(generic.DetailView):
+    model = Song
+    # song = get_object_or_404(Song, pk=song_id)
+    # return render(request, 'songapp/detail.html', {'song': song})
+    template_name = "songapp/detail.html"
